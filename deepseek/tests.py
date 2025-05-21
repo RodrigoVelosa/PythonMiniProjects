a51ba2a5749b15dcf70b7c0f160db2d125f8dd87
Rodrigo Velosa <rodrigofvelosa@gmail.com>
2025-05-21 22:59:45 +0100

    Deepseek health metrics

--- deepseek/tests.py
+++ deepseek/tests.py
@@ -4,10 +4,15 @@ import re
 
 BASE_URL = 'http://127.0.0.1:1234/'
 
+HEALTH_METRICS_CSV_FILE_PATH = 'HealthMetrics.csv'
+
 def post_chat_completions():
+    question = input("Entao?\n")
+    deepseek_question(question)
+
+def deepseek_question(question):
     endpoint = BASE_URL + 'v1/chat/completions'
 
-    question = input("Entao?\n")
     payload = {
         "model": "deepseek-r1-distill-qwen-7b",
         "messages": [
@@ -55,7 +60,6 @@ def post_chat_completions():
 
     print(answer)
 
-
 def get_models():
     endpoint = BASE_URL + 'v1/models'
     response = requests.get(endpoint)
@@ -67,5 +71,15 @@ def get_models():
     generated_response = response.json()
     print(generated_response)
 
+def health_metrics():
+    try:
+        with open(HEALTH_METRICS_CSV_FILE_PATH, 'r') as file:
+            csv_content = file.read()
+    except FileNotFoundError:
+        print('Error: Missing File')
 
-post_chat_completions()
+    question = f"Analyze the below health data: {csv_content}. Please give me a health report and suggest improvements"
+    deepseek_question(question)
+
+
+health_metrics()
